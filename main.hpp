856819055a1664ea2e94409d7bc6e37300f802a0
Bennett <32626837+Bannatte@users.noreply.github.com>
2026-05-17 23:16:32 -0700

    Fix loop conditions and iterator comparisons

--- main.hpp
+++ main.hpp
@@ -21,7 +21,7 @@ void printVector(vector<int>);
 void makeVector(vector<int> &number)
 {
     // TODO: fill the vector with random integers in [0, 20), then sort it.
-    for (int i - 0; i < number.size(); i++) {
+    for (int i = 0; i < number.size(); i++) {
         number[i] = (rand() % 20);
     }
     sort(number.begin(), number.end());
@@ -31,7 +31,7 @@ void insertVector(vector<int> &number, int usernum)
 {
     // TODO: insert usernum into the sorted vector while keeping it sorted.
     vector<int>::iterator iter = number.begin();
-    while ((iter < number.end()) && (*iter <= usernum)) {
+    while ((iter != number.end()) && (*iter <= usernum)) {
         iter++;
     }
     number.insert(iter, usernum);
@@ -42,7 +42,7 @@ int deleteVector(vector<int> &number, int usernum)
     // TODO: erase EVERY occurrence of usernum from the vector.
     int count = 0;
     vector<int>::iterator iter = find(number.begin(), number.end(), usernum);
-    while (iter < number.end()) {
+    while (iter != number.end()) {
         number.erase(iter);
         count++;
 
